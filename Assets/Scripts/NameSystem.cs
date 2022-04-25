3a43f394f191f78b8e94e28576bb65f9799b01a8
Plasma <50627025+PlasmaDeveloper@users.noreply.github.com>
2022-04-25 16:53:17 +0200

    name saved
    name is able to save betweeen games/restarts and last saved name gets displayed in the input field at game start.
    Points dont get saved and you still can play in the background.

--- Assets/Scripts/NameSystem.cs
+++ Assets/Scripts/NameSystem.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using System.IO;
 
 public class NameSystem : MonoBehaviour
 {
     //objects and components
     [SerializeField] private MainManager mainManager;
     [SerializeField] private Text scoreText;
+    [SerializeField] private TMP_InputField inputUsername;
 
     //values
     private string userName;
@@ -17,6 +19,8 @@ public class NameSystem : MonoBehaviour
     void Start()
     {
         SetupGetComponents();
+        LoadName();
+        FillWithSavedName();
     }
 
     // Update is called once per frame
@@ -31,6 +35,11 @@ public class NameSystem : MonoBehaviour
 
     }
 
+    public void FillWithSavedName()
+    {
+        inputUsername.text = userName;
+    }
+
     public void SetName(string input)
     {
         userName = input;
@@ -40,6 +49,36 @@ public class NameSystem : MonoBehaviour
     public void AcceptName()
     {
         scoreText.text = "Best Score : " + userName + " : 0";
+        SaveName();
         mainManager.StartGame();
+    }
+
+    [System.Serializable]
+    class SaveData
+    { 
+        public string userName;
+    }
+
+    public void SaveName()
+    {
+        SaveData data = new SaveData();
+        data.userName = userName;
+
+        string json = JsonUtility.ToJson(data);
+
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    public void LoadName()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            userName = data.userName;
+            SetName(data.userName);
+        }
     }
 }
